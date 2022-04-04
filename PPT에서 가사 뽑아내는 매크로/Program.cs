b530bced4b8d3649a40cb8dd9490f301fc4d18e4
patternplane <bss011202@naver.com>
2022-04-04 19:24:08 +0900

    초기 커밋.

---
+++ PPT에서 가사 뽑아내는 매크로/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Microsoft.Office.Interop.PowerPoint;
+using System.Diagnostics;
+
+namespace PPT에서_가사_뽑아내는_매크로
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+
+            String path;
+
+            Microsoft.Office.Core.FileDialog fd =
+            app.FileDialog[Microsoft.Office.Core.MsoFileDialogType.msoFileDialogFolderPicker];
+            fd.Show();
+            path = fd.SelectedItems.Item(1);
+
+            DirectoryInfo id = new DirectoryInfo(path);
+
+            StringBuilder allLiric = new StringBuilder("");
+
+            foreach (FileInfo file in id.GetFiles())
+            {
+                if ((file.Extension.ToLower().CompareTo(".ppt") == 0)
+                    || (file.Extension.ToLower().CompareTo(".pptx") == 0)
+                    || (file.Extension.ToLower().CompareTo(".pptm") == 0))
+                {
+                    Presentation ppt = app.Presentations.Open(file.FullName);
+
+                    allLiric.Append(file.Name);
+                    foreach (Slide s in ppt.Slides)
+                        foreach (Shape sh in s.Shapes)
+                            if (sh.HasTextFrame != Microsoft.Office.Core.MsoTriState.msoFalse)
+                            {
+                                allLiric.Append(sh.TextFrame.TextRange.Text);
+                                allLiric.Append("\n");
+                            }
+
+                    allLiric.Append("∂\n");
+
+                    ppt.Close();
+                }
+            }
+
+            if (allLiric.Length != 0)
+            {
+                StreamWriter outputFile = new StreamWriter(path + "\\출력.txt", false);
+                outputFile.Write(allLiric);
+                outputFile.Close();
+            }
+            Process.Start("explorer.exe", path);
+        }
+    }
+}
