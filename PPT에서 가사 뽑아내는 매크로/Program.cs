0086496f218713464e02502652adc2d99f7acfc0
patternplane <bss011202@naver.com>
2022-04-05 01:56:20 +0900

    [FIX] 경로 지정 오류 해결
    - 폴더 선택창에서 바탕화면 위치에서 바로 확인 누르면 오류가 뜨는 문제
    - 폴더 선택을 취소할 경우 에러발생 문제

--- PPT에서 가사 뽑아내는 매크로/Program.cs
+++ PPT에서 가사 뽑아내는 매크로/Program.cs
@@ -50,8 +50,11 @@ namespace PPT에서_가사_뽑아내는_매크로
 
             Microsoft.Office.Core.FileDialog fd =
             app.FileDialog[Microsoft.Office.Core.MsoFileDialogType.msoFileDialogFolderPicker];
-            fd.InitialFileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            fd.InitialFileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
             fd.Show();
+
+            if (fd.SelectedItems.Count == 0)
+                return;
 
             path = fd.SelectedItems.Item(1);
 
